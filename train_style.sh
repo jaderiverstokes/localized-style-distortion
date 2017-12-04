36c83d5eaf9d8e5a3822fffd5670004e40fdb604
Ubuntu <ubuntu@ip-172-31-14-29.us-east-2.compute.internal>
2017-12-04 19:49:26 +0000

    Modified script to train 1 epoch only

--- train_style.sh
+++ train_style.sh
@@ -1,2 +1,2 @@
 export STYLE=$1
-python fast-neural-style/neural_style/neural_style.py train --dataset ../train2014 --style-image images/style-images/$STYLE --vgg-model-dir fast-neural-style/neural_style/ --save-model-dir images/save-models/ --epochs 2 --cuda 1
+python fast-neural-style/neural_style/neural_style.py train --dataset ~/training_data --style-image images/style-images/$STYLE --vgg-model-dir fast-neural-style/neural_style/ --save-model-dir images/save-models/ --epochs 1 --cuda 1
